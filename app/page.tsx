8a742e88072d43c5dbbec1a4006ac76b37ecfcd6
emotigom <ahnsangkyoon@gmail.com>
2024-11-21 22:58:36 +0900

    start flagwithdal

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center text-center h-screen bg-green-600">
+      <div className="bg-black text-white py-5 w-full">곰사이트가 곰</div>
+      <div className="flex justify-around w-full bg-orange-500 py-3">
+        <div className="text-white font-bold">마음 편지</div>
+        <div className="text-white font-bold">배리어프리</div>
+        <div className="text-white font-bold">장애인 개인 예산제</div>
+        <div className="text-white font-bold">이동의 자유</div>
+        <div className="text-white font-bold">인식개선</div>
+        <div className="text-white font-bold">편의즐기기</div>
+      </div>
+      <div className="flex-1 w-full bg-green-600 text-[1100px]">🐻</div>
+    </div>
+  );
+}
